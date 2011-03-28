020d82c286f430689fd384a1f2d057558854cd39
mallowlabs <mallowlabs@gmail.com>
2011-03-29 01:14:49 +0900

    mallowlabs's prototype version

--- README.md
+++ README.md
@@ -3,10 +3,17 @@ AsakusaSatellite Chrome Extension
 
 Features
 ----------------
- * Message notification on background. (joined room only)
+ * Message notification on background.
 
 Requirement
 ----------------
  * Chrome 10 or latter
 
+Tasks
+----------------
+ * Multiple servers
+ * Filter rooms
+ * Remove own messages
+ * Get previous messages
+ * Hide notifications if you open tabs for see the web interface
 
--- javascripts/background.js
+++ javascripts/background.js
@@ -2,57 +2,87 @@ $(function(){
     if(!localStorage['api-root']){ return; }
 
     function api(path){
-	return localStorage['api-root']+path;
+        var apiRoot = localStorage['api-root'];
+        if (apiRoot.substr(-1) == "/") {
+            apiRoot = apiRoot.substring(0, apiRoot.length - 1);
+        }
+        return apiRoot + path;
+    }
+    function websocketApi(id){
+        var apiRoot = localStorage['ws-root'];
+        if (apiRoot.substr(-1) == "/") {
+            apiRoot = apiRoot.substring(0, apiRoot.length - 1);
+        }
+        return apiRoot + "/room?id=" + id
     }
     function notify(message){
-	if(message.screen_name != "#{current_user.screen_name}") {
-	    $.fn.desktopNotify({
-		picture: message.profile_image_url,
-		title: message.name,
-		text : (message.attachment != null ? message.attachment.filename : message.body)
-	    });
-	}
+        //if(message.screen_name != "#{current_user.screen_name}") {
+            $.fn.desktopNotify({
+                picture: message.profile_image_url,
+                title: message.name,
+                text : (message.attachment != null ? message.attachment.filename : message.body)
+            });
+        //}
     }
 
+/*
     function getPrevMessage(){
-	console.log(localStorage['last-id']);
-	if(localStorage['last-id']){
-	    $.getJSON(api("/api/v1/user/rooms"),
-		      { 'api_key' : localStorage['api-key'] },
-		      function(json){
-			  $(json.rooms).each(function(_, room){
-			      $.getJSON(api("/api/v1/room/"+room.id+".json"),
-					{ 'api_key' : localStorage['api-key'],
-					  'since_id' : localStorage['last-id'] },
-					function(json){
-					    $(json).each(function(_, m){
-						localStorage['last-id'] = m.id;
-						notify(m);
-					    });
-					});
-			  });
-		      });
-	}
-	$(".message-list").bind('websocket::create', function(_, message){
-	    localStorage['last-id'] = message.id;
-	});
+        console.log(localStorage['last-id']);
+        if(localStorage['last-id']){
+            $.getJSON(api("/api/v1/user/rooms"),
+            { 'api_key' : localStorage['api-key'] },
+            function(json){
+                $(json.rooms).each(function(_, room){
+                    $.getJSON(api("/api/v1/room/"+room.id+".json"),
+                    { 'api_key' : localStorage['api-key'],
+                     'since_id' : localStorage['last-id'] },
+                    function(json){
+                        $(json).each(function(_, m){
+                            localStorage['last-id'] = m.id;
+                            notify(m);
+                        });
+                    });
+                });
+            });
+        }
+        $(".message-list").bind('websocket::create', function(_, message){
+            localStorage['last-id'] = message.id;
+        });
+    }*/
+
+    /** connect to all rooms */
+    function connectToAllRooms() {
+       $.getJSON(api("/api/v1/room/list.json"),
+       function(json) {
+          var i = 0;
+          $(json).each(function (_, m) {
+              connectToRoom(m.id);
+              i++;
+          });
+          setBadgeNumber(i);
+       })
+    }
+    /** connect to a room */
+    function connectToRoom(id){
+        var elem = $(".message-list")
+        elem.webSocket({
+            entry : websocketApi(id)
+        });
+
+        elem.bind('websocket::create', function(_, message){
+            notify(message);
+        });
+    };
+    /** set a number to badge */
+    function setBadgeNumber(i) {
+        chrome.browserAction.setBadgeText({text: String(i)});
     }
 
-    function waitForNewMessage(){
-	var elem = $(".message-list")
-	elem.webSocket({
-	    entry : localStorage['ws-root'] + "/user?api_key="+localStorage['api-key']
-	});
-
-	elem.bind('websocket::create', function(_, message){
-	    notify(message);
-	});
-    };
+    connectToAllRooms();
 
     $.fn.desktopNotify({
-	title:"AsakusaSatellite",
-	text : "Background start"
+        picture: "images/icon48.png",
+        title: "AsakusaSatellite",
+        text : "Background page started."
     });
-    getPrevMessage();
-    waitForNewMessage();
 });
